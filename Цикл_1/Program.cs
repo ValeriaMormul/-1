1f930c831c048fef2f686d2aebf07c9913ffaad9
Valeriia Mormul <152411242+ValeriaMormul@users.noreply.github.com>
2023-12-04 23:26:32 +0200

    Добавьте файлы проекта.

---
+++ Цикл_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Парные числа от 1 к 20 создают ряд: ");
+        int i = 2;
+        while (i <= 20)
+        {
+            Console.Write(i + " ");
+            i += 2;
+        }
+    }
+}
